6533dc517e4931f9521b890ad879eed06b9208a7
Hannah <hvdam@live.nl>
2023-05-11 15:36:34 +0200

    Using @PathVariable instead of @RequestParam

--- rws-api/src/main/java/rws/rwsapi/api/LocationApi.java
+++ rws-api/src/main/java/rws/rwsapi/api/LocationApi.java
@@ -3,9 +3,9 @@ package rws.rwsapi.api;
 import main.dao.Location;
 import main.exception.IncorrectZipcodeException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface LocationApi {
 
-    ResponseEntity<Location> getLocation(@RequestParam int zipcode) throws IncorrectZipcodeException;
+    ResponseEntity<Location> getLocation(@PathVariable int zipcode) throws IncorrectZipcodeException;
 }
--- rws-api/src/main/java/rws/rwsapi/api/LocationController.java
+++ rws-api/src/main/java/rws/rwsapi/api/LocationController.java
@@ -3,8 +3,8 @@ package rws.rwsapi.api;
 import main.dao.Location;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rws.rwsapi.service.LocationService;
 
@@ -18,8 +18,8 @@ public class LocationController implements LocationApi {
         this.locationService = locationService;
     }
 
-    @GetMapping
-    public ResponseEntity<Location> getLocation(@RequestParam int zipcode) {
+    @GetMapping("/{zipcode}")
+    public ResponseEntity<Location> getLocation(@PathVariable int zipcode) {
         Location location = locationService.findLocation(zipcode);
         return ResponseEntity.ok(location);
     }
